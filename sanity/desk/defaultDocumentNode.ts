703c377c02c39fc18b5f2898c140c3f9737b49e7
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-09-22 12:48:10 -0300

    Show the edited page in preview

--- sanity/desk/defaultDocumentNode.ts
+++ sanity/desk/defaultDocumentNode.ts
@@ -1,31 +1,39 @@
 import { DefaultDocumentNodeResolver } from "sanity/desk";
 import Iframe from "sanity-plugin-iframe-pane";
+import { SanityDocument } from "next-sanity";
+
+function getPreviewUrl(doc: SanityDocument) {
+  console.log(`printer: ${doc}`);
+  return doc?.slug?.current
+    ? `${"http://localhost:3000"}/${doc.slug.current}`
+    : "http://localhost:3000";
+}
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (
   S,
   { schemaType }
 ) => {
   switch (schemaType) {
-    case "post":
+    case "resume":
       return S.document().views([
         S.view.form(),
         S.view
           .component(Iframe)
           .options({
-            url: "http://localhost:3000/api/preview",
+            url: (doc: SanityDocument) => getPreviewUrl(doc),
           })
           .title("Preview"),
       ]);
     default:
-      // return S.document().views([S.view.form()]);
-      return S.document().views([
-        S.view.form(),
-        S.view
-          .component(Iframe)
-          .options({
-            url: "http://localhost:3000/api/preview",
-          })
-          .title("Preview"),
-      ]);
+      return S.document().views([S.view.form()]);
+    // return S.document().views([
+    //   S.view.form(),
+    //   S.view
+    //     .component(Iframe)
+    //     .options({
+    //       url: "http://localhost:3000/api/preview",
+    //     })
+    //     .title("Preview"),
+    // ]);
   }
 };
